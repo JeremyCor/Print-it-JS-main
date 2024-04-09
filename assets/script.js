58c0728a3b4e38d72853421a791073f0e0312b14
Jeremy <jeremycormier.imp@gmail.com>
2024-04-09 14:30:59 +0200

    Slide fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,34 @@ const slides = [
 	}
 ]
 
-// Sélection des éléments du DOM nécessaires
+// --- I - définir les éléments du DOM à utiliser ---
 const carrouselImages = document.querySelectorAll('#banner .banner-img');
 const dotsContainer = document.querySelector('#banner .dots');
-  // const tagLine = document.querySelector('#banner p');
-// Variable pour suivre l'index de la diapositive actuellement affichée
+// établir la diapo de départ
 let currentSlideIndex = 0;
 
-// Boucle pour créer un point pour chaque image du carrousel
+
+// --- II - Créer les eventListener sur les flèches ---
+
+// 1 - Sélection des flèches du carrousel
+const leftArrow = document.querySelector('#banner .arrow_left');
+const rightArrow = document.querySelector('#banner .arrow_right');
+
+// 2 - Ajout d'un gestionnaire d'événement click à la flèche gauche
+leftArrow.addEventListener('click', () => {
+  // Appel de la fonction changeSlides avec l'argument 'prev' pour passer à la diapositive précédente
+  changeSlides('prev');
+});
+
+// 3 - Ajout d'un gestionnaire d'événement click à la flèche droite
+rightArrow.addEventListener('click', () => {
+  // Appel de la fonction changeSlides avec l'argument 'next' pour passer à la diapositive suivante
+  changeSlides('next');
+
+});
+
+
+// --- III - Boucle pour créer un point pour chaque image du carrousel ---
 slides.forEach((slide, index) => {
 	const dot = document.createElement('div');
 	dot.classList.add('dot');
@@ -35,33 +55,51 @@ slides.forEach((slide, index) => {
 	// Ajout du point au conteneur des points du carrousel
 	dotsContainer.appendChild(dot);
   });
+
   
+// Fonction pour mettre à jour la diapositive en fonction de l'index fourni
+function updateSlide(index) {
+  carrouselImages.forEach((image, i) => {
+    // Mise à jour de chaque image du carrousel avec la nouvelle image et l'attribut alt
+    image.src = `./assets/images/slideshow/${slides[index].image}`;
+    image.alt = `Banner Print-it${slides[index].image}`;
+  });
+
+  // Mise à jour de la ligne de texte correspondante à la diapositive
+  const tagLine = document.querySelector('#banner p');
+  tagLine.innerHTML = slides[index].tagLine;
+
+  // --- IV - Mise à jour des points du carrousel ---
+  const dots = document.querySelectorAll('#banner .dots .dot');
+  dots.forEach((dot, i) => {
+    // Ajout de la classe dot_selected pour le point correspondant à la diapositive actuelle
+    if (i === index) {
+      dot.classList.add('dot_selected');
+    } else {
+      dot.classList.remove('dot_selected');
+    }
+  });
+}
+
+function changeSlides(direction) {
+  if (direction === 'next') {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  } else if (direction === 'prev') {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+  }
+  updateSlide(currentSlideIndex);
+}
+
 
 // Sélection de tous les points du carrousel
 const dots = document.querySelectorAll('#banner .dots .dot');
 dots.forEach((dot, index) => {
-  // event listener à chaque point
+  // Ajout d'un event listener à chaque point
   dot.addEventListener('click', () => {
-    // MaJ diapo en fonction de l'index du point cliqué
+    // Mise à jour de la diapositive en fonction de l'index du point cliqué
     currentSlideIndex = index;
     updateSlide(currentSlideIndex);
   });
-});
-
-// flèches du carrousel
-const leftArrow = document.querySelector('#banner .arrow_left');
-const rightArrow = document.querySelector('#banner .arrow_right');
-
-//effet du click à la flèche gauche
-leftArrow.addEventListener('click', () => {
-  // changement du slide pour passer à la diapositive précédente
-  changeSlides('prev');
-});
-
-//effet du click à la flèche droite
-rightArrow.addEventListener('click', () => {
-  // changement du slide pour passer à la diapositive suivante
-  changeSlides('next');
 });
 
 console.log(carrouselImages, dotsContainer, leftArrow, rightArrow);
