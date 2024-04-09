115c9fb37685d25d8143eb4520c3598d38d5187d
Jeremy <jeremycormier.imp@gmail.com>
2024-04-09 12:59:17 +0200

    Ajout des points du carroussel

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,38 @@ const slides = [
 	}
 ]
 
-// Sélection des flèches du carrousel
+// Sélection des éléments du DOM nécessaires
+const carrouselImages = document.querySelectorAll('#banner .banner-img');
+const dotsContainer = document.querySelector('#banner .dots');
+  // const tagLine = document.querySelector('#banner p');
+// Variable pour suivre l'index de la diapositive actuellement affichée
+let currentSlideIndex = 0;
+
+// Boucle pour créer un point pour chaque image du carrousel
+slides.forEach((slide, index) => {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	// Ajout de la classe dot_selected pour le premier point (diapositive actuelle)
+	if (index === 0) {
+	  dot.classList.add('dot_selected');
+	}
+	// Ajout du point au conteneur des points du carrousel
+	dotsContainer.appendChild(dot);
+  });
+  
+
+// Sélection de tous les points du carrousel
+const dots = document.querySelectorAll('#banner .dots .dot');
+dots.forEach((dot, index) => {
+  // event listener à chaque point
+  dot.addEventListener('click', () => {
+    // MaJ diapo en fonction de l'index du point cliqué
+    currentSlideIndex = index;
+    updateSlide(currentSlideIndex);
+  });
+});
+
+// flèches du carrousel
 const leftArrow = document.querySelector('#banner .arrow_left');
 const rightArrow = document.querySelector('#banner .arrow_right');
 
@@ -32,3 +63,5 @@ rightArrow.addEventListener('click', () => {
   // changement du slide pour passer à la diapositive suivante
   changeSlides('next');
 });
+
+console.log(carrouselImages, dotsContainer, leftArrow, rightArrow);
